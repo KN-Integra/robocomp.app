980dd623813c1c6ee2fe9edb1c64e8baddf35c55
Octoturge <iwo.strzebonski@gmail.com>
2025-04-03 09:13:16 +0200

    Update index.get.ts

--- server/api/stats/index.get.ts
+++ server/api/stats/index.get.ts
@@ -45,8 +45,10 @@ export default defineEventHandler(async (event): Promise<StatsResponse | NuxtErr
   const query = getQuery<Partial<StatsQuery>>(event)
 
   // TEMP: we are showing archival data for now
-  const year = query.year || new Date().getFullYear().toString()
   // TODO: Revisit this temporary change when archival data is no longer needed
+  const year = 2024
+  // const year = query.year || new Date().getFullYear().toString()
+  
   if (!year.match(/^20\d{2}$/)) {
     return createError({
       statusCode: 400,
