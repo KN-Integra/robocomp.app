9170212d4ccf325f4d2095d6e51f472158f2c88d
Octoturge <iwo.strzebonski@gmail.com>
2025-10-08 00:16:00 +0200

    Add TSHIRT_END_DATE constant for registration

--- settings/constants.ts
+++ settings/constants.ts
@@ -1,4 +1,5 @@
 export const APP_TITLE = 'Festiwal Robotyki ROBOCOMP'
 export const EVENT_DATE = '2025-10-18'
 export const REGISTRATION_END_DATE = '2025-10-11T23:59:59'
+export const TSHIRT_END_DATE = '2025-10-07T23:59:59'
 export const GALLERY_IMG_SIZE_PX = 500
